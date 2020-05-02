12339658383789f810c1f2a7d827e27be9ce53a7
ninatchang <ninatchang@berkeley.edu>
2020-05-01 17:27:28 -0700

    oops forgot to put it in install apps

--- reminder_app/reminder_app/settings.py
+++ reminder_app/reminder_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reminders', 
+    'user_management',
 ]
 
 MIDDLEWARE = [

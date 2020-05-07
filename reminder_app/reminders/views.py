c5339b48a85f4c19b1a5a700a4a32eb88ecb9d5d
ninatchang <ninatchang@berkeley.edu>
2020-05-06 21:13:46 -0700

    hopefully ebtter style but tbh not really

--- reminder_app/reminders/views.py
+++ reminder_app/reminders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from reminders.models import Reminder
 from django.contrib.auth.decorators import login_required
 from django.http import QueryDict
@@ -29,28 +29,15 @@ def createTask(request):
         currentTask = Reminder.objects.create(creator=request.user, body=taskBody, dueTimeStamp=taskDueDate)
 
     reminders = Reminder.objects.filter(creator=request.user).order_by('dueTimeStamp')
-    remainingTime = []
-    for r in reminders:
-        if r.dueTimeStamp:
-            remainingTime.append(humanfriendly.format_timespan(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None)))
-        else:
-            remainingTime.append("No set due date! Take your time!!")
-    return render(request, 'reminders.html', {"reminders": zip(reminders, remainingTime), "user": request.user if not request.user.is_anonymous else None, "suggestion" : ""})
-
+    return redirect("/")
+    
 @login_required(login_url="/login/")
 def deleteTask(request, reminderId):
     task = Reminder.objects.get(reminderId = reminderId)
     task.delete()
     
     reminders = Reminder.objects.filter(creator=request.user).order_by('dueTimeStamp')
-    remainingTime = []
-    for r in reminders:
-        if r.dueTimeStamp:
-            remainingTime.append(humanfriendly.format_timespan(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None)))
-        else:
-            remainingTime.append("No set due date! Take your time!!")
-    return render(request, 'reminders.html', {"reminders": zip(reminders, remainingTime), "user": request.user if not request.user.is_anonymous else None, "suggestion" : ""})
-
+    return redirect("/")
 
 @login_required(login_url="/login/")
 def chooseRandom(request):

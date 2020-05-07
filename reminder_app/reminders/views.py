8f61845980c3b75e60d1e7df40502328b59f0716
ninatchang <ninatchang@berkeley.edu>
2020-05-06 20:48:04 -0700

    allow for null dates

--- reminder_app/reminders/views.py
+++ reminder_app/reminders/views.py
@@ -3,13 +3,17 @@ from reminders.models import Reminder
 from django.contrib.auth.decorators import login_required
 from django.http import QueryDict
 from datetime import datetime
+import humanfriendly
 
 # Create your views here.
 def remindersPage(request):
     reminders = Reminder.objects.filter(creator=request.user).order_by('dueTimeStamp')
     remainingTime = []
     for r in reminders:
-        remainingTime.append(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None))
+        if r.dueTimeStamp:
+            remainingTime.append(humanfriendly.format_timespan(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None)))
+        else:
+            remainingTime.append("No set due date! Take your time!!")
     return render(request, 'reminders.html', {"reminders": zip(reminders, remainingTime), "user": request.user if not request.user.is_anonymous else None})
 
 @login_required(login_url="/login/")
@@ -17,12 +21,19 @@ def createTask(request):
     queries = QueryDict(request.body)
     taskBody = queries["taskBody"]
     taskDueDate = queries["reminderDueDate"]
-    currentTask = Reminder.objects.create(creator=request.user, body=taskBody, dueTimeStamp=taskDueDate)
+    currentTask = None
+    if (taskDueDate == ''):
+        currentTask = Reminder.objects.create(creator=request.user, body=taskBody)
+    else:
+        currentTask = Reminder.objects.create(creator=request.user, body=taskBody, dueTimeStamp=taskDueDate)
 
     reminders = Reminder.objects.filter(creator=request.user).order_by('dueTimeStamp')
     remainingTime = []
     for r in reminders:
-        remainingTime.append(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None))
+        if r.dueTimeStamp:
+            remainingTime.append(humanfriendly.format_timespan(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None)))
+        else:
+            remainingTime.append("No set due date! Take your time!!")
     return render(request, 'reminders.html', {"reminders": zip(reminders, remainingTime), "user": request.user if not request.user.is_anonymous else None})
 
 @login_required(login_url="/login/")
@@ -33,5 +44,8 @@ def deleteTask(request, reminderId):
     reminders = Reminder.objects.filter(creator=request.user).order_by('dueTimeStamp')
     remainingTime = []
     for r in reminders:
-        remainingTime.append(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None))
+        if r.dueTimeStamp:
+            remainingTime.append(humanfriendly.format_timespan(r.dueTimeStamp.replace(tzinfo=None) - datetime.now().replace(tzinfo=None)))
+        else:
+            remainingTime.append("No set due date! Take your time!!")
     return render(request, 'reminders.html', {"reminders": zip(reminders, remainingTime), "user": request.user if not request.user.is_anonymous else None})
